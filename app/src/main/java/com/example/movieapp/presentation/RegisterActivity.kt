31fb3caf831551ebbe0c8ca0257e071878c3f041
Atilla ÇALIKUŞU <calikusu.atilla@gmail.com>
2024-07-29 16:58:48 +0300

    register xml kodlamaları yapıldı.

--- app/src/main/java/com/example/movieapp/presentation/RegisterActivity.kt
+++ app/src/main/java/com/example/movieapp/presentation/RegisterActivity.kt
@@ -2,25 +2,25 @@ package com.example.movieapp.presentation
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.movieapp.R
-import com.example.movieapp.databinding.ActivityLoginBinding
+import com.example.movieapp.databinding.ActivityRegisterBinding
 
-class LoginActivity : BaseActivity() {
-
-    private lateinit var binding: ActivityLoginBinding
+class RegisterActivity : BaseActivity() {
+    private lateinit var binding: ActivityRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityLoginBinding.inflate(layoutInflater)
+        val binding = ActivityRegisterBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
 
-        binding.registerTxt.setOnClickListener {
-            val intent = Intent(this@LoginActivity,RegisterActivity::class.java)
+        binding.loginTxt.setOnClickListener {
+            val intent = Intent(this@RegisterActivity,LoginActivity::class.java)
             startActivity(intent)
             finish()
         }
