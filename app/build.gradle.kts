c89610a7e0e24876d3df0072d48e7d93219a26b9
Atilla ÇALIKUŞU <calikusu.atilla@gmail.com>
2024-07-31 00:24:19 +0300

    DetailActivty Xml kodaları yazıldı.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -115,4 +115,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 
+    //BlurView
+    implementation (libs.blurview)
+
 }
